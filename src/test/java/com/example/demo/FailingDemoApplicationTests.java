094016751820f58dec6745eb05d1d2965b6d3ee3
stoetti <stoettinger@unisoftwareplus.com>
2021-01-25 15:07:00 +0100

    add failing test

--- src/test/java/com/example/demo/FailingDemoApplicationTests.java
+++ src/test/java/com/example/demo/FailingDemoApplicationTests.java
@@ -1,29 +1,19 @@
 package com.example.demo;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.cloud.function.context.FunctionRegistry;
-import org.springframework.cloud.task.listener.TaskEventAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,9 +21,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 //@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 @Testcontainers
-@ContextConfiguration(initializers = DemoApplicationTests.AppInitializer.class)
-@EnableAutoConfiguration(exclude = TaskEventAutoConfiguration.class)
-class DemoApplicationTests {
+@ContextConfiguration(initializers = FailingDemoApplicationTests.AppInitializer.class)
+class FailingDemoApplicationTests {
 
     static class AppInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
